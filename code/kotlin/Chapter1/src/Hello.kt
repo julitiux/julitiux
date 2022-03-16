3665c2bfb67a11b4f1a7ef56397080b517e073fa
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-03-15 22:58:55 -0600

    add first execies of chapter 1

--- code/kotlin/Chapter1/src/Hello.kt
+++ code/kotlin/Chapter1/src/Hello.kt
@@ -1,3 +1,19 @@
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
   println("Hello World")
+
+//  -----------------------------
+  val emptyList = emptyList<Int>()
+  println("emptyList.any() is ${emptyList.any()}")
+
+  val nonEmptyList = listOf<Int>(1, 2, 3)
+  println("nonEmptyList.any() is ${nonEmptyList.any()}")
+
+//  -----------------------------
+  val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+  val firstOdd = numbers.find { it % 2 == 0 }
+  val firstLast = numbers.findLast { it % 2 == 0 }
+
+  println(firstOdd)
+  println(firstLast)
+
 }
